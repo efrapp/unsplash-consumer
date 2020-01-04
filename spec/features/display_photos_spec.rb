b180d89d73006de08fdcb6aece90cb3964c55d3f
Efrain Pinto <efrapp@gmail.com>
2020-01-04 01:42:53 -0500

    Add feature test to check photos displayed in the page

--- spec/features/display_photos_spec.rb
+++ spec/features/display_photos_spec.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "DisplayPhotos", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'Display photos', type: :feature do
+  scenario 'photos without search query' do
+    VCR.use_cassette('unsplash/photos', record: :new_episodes) { visit photos_path }
+
+    expect(current_path).to eq photos_path
+
+    expect(page).to have_css('.photo', count: 10)
+  end
 end
